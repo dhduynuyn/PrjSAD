808f89fc4899e432dcece06a146498b55e788629
Nguyen Kim Khanh <128380887+kknguyen214@users.noreply.github.com>
2025-03-14 20:07:23 +0700

    story infomation: genre, author, group, state, description

--- crawling/crawling/spiders/monkey_chapter.py
+++ crawling/crawling/spiders/monkey_chapter.py
@@ -7,6 +7,7 @@ class MonkeyStoryCrawler(scrapy.Spider):
     name = "monkey_story_crawler"
     chapter_urls = [] # List lưu danh sách URL chương truyện
     
+    
     def start_requests(self):
         url = getattr(self, 'url', None)  # Get URL from command line argument
         if url:
@@ -14,7 +15,16 @@ class MonkeyStoryCrawler(scrapy.Spider):
 
     def parse(self, response):
         self.chapter_urls = response.css(".list-chapters .episode-title a::attr(href)").getall() 
-        self.log(f"Scraped URL: {response.url}")
+        self.genre = response.css(".cate-item::text").getall()
+        self.author = response.css(".col-sm-9::text").getall()[3]
+        self.groups = response.css(".col-sm-9 .btn.btn-sm.btn-info::text").getall()
+        self.groups = [group.strip() for group in self.groups]
+        self.state = response.css(".col-sm-9::text").getall()[9]
+        self.description = response.css(".story-description p::text").getall()
+        self.description = "\n".join(self.description).strip()
         
-        for chapter_url in self.chapter_urls:
-            print(chapter_url)
+        print(self.description)
+        # print("genre: ", self.genre)
+        # print("author: ", self.author)
+        # print("groups: ", self.groups)
+        # print("state: ", self.state)
